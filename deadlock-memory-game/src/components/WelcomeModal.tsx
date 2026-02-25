fc206b2aa95891874edf563d91253489428aa790
Stingos <Justmo10@gmail.com>
2026-02-25 15:14:35 -0500

    add images for welcome, win, and lose

--- deadlock-memory-game/src/components/WelcomeModal.tsx
+++ deadlock-memory-game/src/components/WelcomeModal.tsx
@@ -13,6 +13,7 @@ export function WelcomeModal({ status, onStart }: WelcomeModalProps) {
     return (
         <div className="fixed inset-0 flex items-center justify-center bg-black/70 z-50">
             <div className="bg-gray-800 rounded-xl p-8 flex flex-col items-center gap-4 shadow-2xl">
+                <img src="/src/assets/Viscous_card.png" alt="Viscous Portrait" />
                 <h1>Help Viscous remember his new friends!</h1>
                 <button onClick={onStart}>Start Game</button>
             </div>
