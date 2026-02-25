a84a4f55ecdcf92ac7712f69045e880987ca3e6e
Stingos <Justmo10@gmail.com>
2026-02-25 04:22:19 -0500

    add api calls

---
+++ deadlock-memory-game/src/utils/api.ts
@@ -0,0 +1,22 @@
+import { resourceUsage } from "process";
+import { Hero } from "../types"
+
+const baseURL = "https://assets.deadlock-api.com/v2/heroes/"
+
+export async function fetchHero(id: number): Promise<Hero> {
+    const res = await fetch(baseURL + id);
+    const data = await res.json();
+    return {
+        id: data.id,
+        name: data.name,
+        images: {
+            icon_hero_card: data.images.icon_hero_card
+        }
+    };
+}
+
+export async function fetchHeroes(ids: number[]): Promise<Hero[]> {
+    const promises = ids.map(id => fetchHero(id));
+    const results = await Promise.all(promises);
+    return results;
+}
