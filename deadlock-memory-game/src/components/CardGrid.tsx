baf9fc3385e500eee9c6421b31bab9d96c4f2372
Stingos <Justmo10@gmail.com>
2026-02-25 05:25:27 -0500

    add card grid

---
+++ deadlock-memory-game/src/components/CardGrid.tsx
@@ -0,0 +1,21 @@
+import { Hero } from "../types"
+import { Card } from "./Card";
+
+type CardGridProps = {
+    heroes: Hero[];
+    onClick: (id: number) => void;
+}
+
+export function CardGrid({ heroes, onClick }: CardGridProps){
+    return (
+        <div>
+            {heroes.map(hero => (
+                <Card
+                key={hero.id}
+                hero={hero}
+                onClick={() => onClick(hero.id)}
+                />
+            ))}
+        </div>
+    )
+}
